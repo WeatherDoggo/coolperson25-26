8fc188f3efcdd25b6aa8c06d6b7bbc0d22beb40b
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-04-23 14:15:56 -0400

    Update masterscript.sh

--- masterscript.sh
+++ masterscript.sh
@@ -1,1 +1,14 @@
-
+#!/bin/bash
+LOG = ______
+function print() {
+  echo "$1" | sudo tee -a $LOG 
+}
+print "HAS UPDATE, UPGRADE, & AUTOREMOVE BEEN RUN?"
+read runconfirm
+if [[runconfirm == "yes" || $runconfirm == "y" ]];
+then
+  print "Proceeding..."
+else
+  print "Exiting..."
+  exit
+fi
