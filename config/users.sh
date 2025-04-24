c857718cee52a692a3937a4294742816732f7ac9
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-04-24 12:52:10 +0000

    Update users.sh

--- config/users.sh
+++ config/users.sh
@@ -4,7 +4,10 @@ function print() {
   echo "$1" | sudo tee -a $LOG 
 }
 #Collect the users on the VM
-USERS=`$(cut -d':' -f1,6 /etc/passwd | grep '/home/' | cut -d':' -f1)`
+print "What is your username?"
+read myusername
+
+USERS=`$(grep '/home/' | cut -d':' -f1 | grep -vE '^$myusername')`
 print "Users found on VM: $USERS"
 #Collect the user info given out by Cyberpatriot
 print "Copy and paste the list of the authorized user/password list here:"
