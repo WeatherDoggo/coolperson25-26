e8f4aa984a190303d34c5e2c5f5cf99166fa8240
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-04-24 09:12:19 -0400

    Update users.sh

--- config/users.sh
+++ config/users.sh
@@ -6,13 +6,14 @@ function print() {
 #Collect the users on the VM, and exclude the one i'm not supposed to edit.
 print "What is your username?"
 read myusername
-USERS=`(cut -d':' -f1,6 /etc/passwd | grep '/home/' | cut -d':' -f1 | grep -vE "^${myusername}")`
-echo -e "Users found on VM:\n$USERS" | sudo tee -a $LOG
+vmusers=`(cut -d':' -f1,6 /etc/passwd | grep '/home/' | cut -d':' -f1 | grep -vE "^${myusername}")`
+echo -e "Users found on VM:\n$vmusers" | sudo tee -a $LOG
 
 #Collect the user info given out by Cyberpatriot
 print "Copy and paste the list of the authorized user/password list here:"
 read givenuserlist
-
+authadmins=``
+authusers=``
 #Compare list of authorized users to the users on the VM. If one is found, append it to a list of usernames.
 print "The following unauthorized users were found: ____"
 print "Would you like to remove them?"
