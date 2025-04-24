cf8cb3eef2f999178ed05d368b663fa1c6118aec
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-04-24 12:35:25 -0400

    Create backup.sh

---
+++ logs/backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+LOG = .mylog
+function print() {
+  echo "$1" | sudo tee -a $LOG 
+}
+#Makes a copy of all important files and saves them to a backups folder
+mkdir -pv backups
+chmod 777 backups
+
+function backup {
+  cp $1 backups/$(basename "$1")
+  chmod 777 backups/$(basename "$1")
+}
+
+crontab -l > backups/cronjobs.txt
+backup /etc/group
+backup /etc/passwd
+backup /etc/pam.d/common-password
+backup /etc/login.defs
+backup /etc/pam.d/common-auth
+backup /etc/security/faillock.conf
+backup /etc/security/pwquality.conf
+backup /etc/sysctl.conf
+print "Critical files backed up. More files may need to conditionally be backed up depending on the application!"
