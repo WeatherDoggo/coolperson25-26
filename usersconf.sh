05c790abc38d2d794d15e7d0f4cc40dff453a628
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-06-01 15:30:39 -0400

    Update usersconf.sh

--- usersconf.sh
+++ usersconf.sh
@@ -8,8 +8,7 @@ function print() {
 print "What is your username?"
 read myusername
 vmusers=`(grep -v 'nologin' /etc/passwd | cut -d':' -f1,6 | grep 'home' | cut -d':' -f1 | grep -v "${myusername}")`
-echo -e "Users found on VM:\n$vmusers" | sudo tee -a $LOG
-echo -e "\n"
+echo -e "\nUsers found on VM:\n$vmusers" | sudo tee -a $LOG
 
 #Collect the user info given out by Cyberpatriot
 print "Copy and paste the list of the authorized admin user/password list here, then press Ctrl + D:"
@@ -49,7 +48,7 @@ for user in "${user_lines[@]}"; do
   fi
 done
 print "\nAuthorized Administrators: ${authadmins[*]}"
-print "Authorized Users: ${authusers[*]}"
+print "\nAuthorized Users: ${authusers[*]}"
 
 #Compare list of authorized users to the users on the VM. If one is found, append it to a list of usernames.
 IFS=$'\n' read -r -d '' -a vm_user_array <<< "$vmusers"
