6f5a09d819930d1c66d75cd209c5355eb5461055
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-06-04 13:08:48 -0400

    Update fileperms.sh

--- fileperms.sh
+++ fileperms.sh
@@ -5,3 +5,12 @@ print "/etc/shadow permissions configured."
 
 chmod 600 /etc/sysctl.conf >> $LOG
 print "sysctl.conf permissions configured."
+
+#Password related
+chown root:root /etc/security/faillock.conf
+chmod og-rwx /etc/security/faillock.conf
+print "faillock.conf permissions configured."
+
+chown root:root /etc/security/pwquality.conf
+chmod og-rwx /etc/security/pwquality.conf
+print "pwquality.conf permissions configured."
