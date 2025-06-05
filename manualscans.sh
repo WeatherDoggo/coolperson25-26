2c008aa73beab1f3c162ee8fa7bcd536cc733460
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-06-05 16:28:39 -0400

    Update manualscans.sh

--- manualscans.sh
+++ manualscans.sh
@@ -4,48 +4,50 @@ function print() {
   echo -e "$1" | sudo tee -a "$LOG" 
 }
 
+mkdir ./scans/
+chmod 777 ./scans/
 print "Running scans and sending results to /scans/..."
 
-print "Applications with hack or crack in the name (remove these):\n" > /scans/hackcrack.txt
-dpkg -l | grep -E 'hack|crack' | sudo tee -a $LOG >> /scans/hackcrack.txt
+print "Applications with hack or crack in the name (remove these):\n" > ./scans/hackcrack.txt
+dpkg -l | grep -E 'hack|crack' | sudo tee -a $LOG >> ./scans/hackcrack.txt
 print "Apps with hack or crack have been scanned for."
 
-ss -tulnp | sudo tee -a $LOG > /scans/ss_-tulnp.txt
+ss -tulnp | sudo tee -a $LOG > ./scans/ss_-tulnp.txt
 print "Ran ss -tulnp to scan listening ports."
 
-ps aux | sudo tee -a $LOG > /scans/ps_aux.txt
+ps aux | sudo tee -a $LOG > ./scans/ps_aux.txt
 print "Ran ps aux to see running processes."
 
-systemctl list-unit-files --type=service | sudo tee -a $LOG > /scans/list-unit-files.txt
+systemctl list-unit-files --type=service | sudo tee -a $LOG > ./scans/list-unit-files.txt
 print "Ran systemctl list-unit-files to see all processes on the VM."
 
-lsof -i -P -n | sudo tee -a $LOG > /scans/lsof.txt
+lsof -i -P -n | sudo tee -a $LOG > ./scans/lsof.txt
 print "Ran lsof to list all open network connections and the processes that opened them."
 
-print "PIDpaths" | sudo tee -a $LOG > /scans/PIDpaths.txt
+print "PIDpaths" | sudo tee -a $LOG > ./scans/PIDpaths.txt
 for pid in $(ls /proc | grep -E '^[0-9]+$'); do 
   if [ -e "/proc/$pid/exe" ];
-    then print "PID: $pid, Command: $(readlink -f /proc/$pid/exe)" >> /scans/PIDpaths.txt; 
+    then print "PID: $pid, Command: $(readlink -f /proc/$pid/exe)" >> ./scans/PIDpaths.txt; 
     fi 
   done
 print "Resolved all of the executible paths of PIDS in PIDpaths.txt. Look for ones in /tmp, /dev/shm, or ones that have been deleted but are still running."
 
-find / -type f -mtime -7 -print0 | xargs -0 ls -lt | sudo tee -a $LOG > /scans/modifiedfiles.txt
+find / -type f -mtime -7 -print0 | xargs -0 ls -lt | sudo tee -a $LOG > ./scans/modifiedfiles.txt
 print "Listed all files modified in the last 7 days."
 
-find / -type f -perm /6000 -ls | sudo tee -a $LOG > /scans/binaryconfigs.txt
+find / -type f -perm /6000 -ls | sudo tee -a $LOG > ./scans/binaryconfigs.txt
 print "Listed all SUID/SGID binaries in binaryconfigs.txt to check for misconfiguration (privilege escalation)."
 #Finds SUID/SGID binaries. Misconfigured SUID/SGID binaries can be exploited for privilege escalation.
 #Look for: Any SUID/SGID binaries that are not standard system binaries or are in unexpected locations.
 
-find / -type f -name ".*" -ls | sudo tee -a $LOG > /scans/hiddenfiles.txt
+find / -type f -name ".*" -ls | sudo tee -a $LOG > ./scans/hiddenfiles.txt
 print "Listed all hidden files in /hiddenfiles.txt."
 #Finds hidden files. Malware often hides its components.
 
-cat /etc/hosts | sudo tee -a $LOG > /scans/hostentries.txt
+cat /etc/hosts | sudo tee -a $LOG > ./scans/hostentries.txt
 #Check for any unusual entries that redirect legitimate traffic to malicious IPs.
 
-cat /etc/resolv.conf | sudo tee -a $LOG > /scans/suspiciousDNS.txt
+cat /etc/resolv.conf | sudo tee -a $LOG > ./scans/suspiciousDNS.txt
 #Check for suspicious DNS servers that could be redirecting traffic.
 
 #Malware-Specific Tools:
