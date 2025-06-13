6f280f85889c507bce05cd9a5ad3534ce525df9b
UrProbablyBetter <andrew.c.patrone@gmail.com>
2025-06-13 14:13:20 -0400

    Update ssh.sh

--- conditional/ssh.sh
+++ conditional/ssh.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+LOG=./logs/main.log
+function print() {
+  echo "$1" | sudo tee -a $LOG 
+}
+
+if [[ $sshneeded == "yes" || $sshneeded == "y" ]];
+then
+	cp importfiles/sshd_config /etc/ssh/sshd_config
+	chmod u-x,og-rwx /etc/ssh/sshd_config
+	chown root:root /etc/ssh/sshd_config
+	print "sshd_config permissions configured."
+ 	systemctl restart sshd >> $LOG
+  print "SSH restarted."
+	print "SSH default port changed, PermitRootLogin set to no, MaxAuthTries set to 3, Client closes after 4 minutes inactive, LoginGraceTime set to 20, PermitEmptyPasswords is set to no, HostBasedAuthentication set to no, and StrictModes is set to yes."
+ 	#manualtask "Optional SSH tasks include MaxSessions, TCPKeepAlive, & changing default port."
+else
+	apt-get purge openssh-server openssh-client -y -qq >> $LOG
+	ufw deny ssh >> $LOG
+	printlog "SSH removed and SSH port closed."
+fi
