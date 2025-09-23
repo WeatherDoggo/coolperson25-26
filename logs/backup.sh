b50f5022ba79e3022f917a8aa32d230aee767ccb
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-09-23 12:23:18 -0400

    Update backup.sh

--- logs/backup.sh
+++ logs/backup.sh
@@ -8,7 +8,7 @@ function backup {
   chmod 777 ../backups/$(basename "$1")
 }
 
-crontab -l > backups/cronjobs.txt
+crontab -l > backups/cronjobs.txt #FIX
 backup /etc/group
 backup /etc/passwd
 backup /etc/pam.d/common-password
@@ -17,5 +17,7 @@ backup /etc/pam.d/common-auth
 backup /etc/security/faillock.conf
 backup /etc/security/pwquality.conf
 backup /etc/sysctl.conf
+if $OS == "Mint"; then
 backup /etc/linuxmint/mintupdate.conf
+fi
 print "Critical files backed up. More files may need to conditionally be backed up depending on the application!"
