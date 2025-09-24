b06b1d18845a62cb6c8dda55b0687b830d864590
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-09-24 11:59:36 -0400

    Update ssh.sh

--- conditional/ssh.sh
+++ conditional/ssh.sh
@@ -6,6 +6,10 @@ function print() {
 
 if [[ $sshneeded == "yes" || $sshneeded == "y" ]];
 then
+	cp /etc/ssh/sshd_config ../backups/sshd_config
+	chmod 777 ../backups/sshd_config
+	print "sshd_config backed up."
+	
 	cp importfiles/sshd_config /etc/ssh/sshd_config
 	chmod u-x,og-rwx /etc/ssh/sshd_config
 	chown root:root /etc/ssh/sshd_config
@@ -13,7 +17,7 @@ then
  	systemctl restart sshd >> $LOG
 	print "SSH restarted."
 	print "SSH default port changed, PermitRootLogin set to no, MaxAuthTries set to 3, Client closes after 4 minutes inactive, LoginGraceTime set to 20, PermitEmptyPasswords is set to no, HostBasedAuthentication set to no, and StrictModes is set to yes."
- 	#Optional SSH tasks include MaxSessions, TCPKeepAlive, & changing default port.
+ 	print "Optional SSH tasks include MaxSessions, TCPKeepAlive, & changing default port."
 else
 	apt-get purge openssh-server openssh-client -y -qq >> $LOG
 	ufw deny ssh >> $LOG
