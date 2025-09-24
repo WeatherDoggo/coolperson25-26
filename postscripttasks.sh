fc0a98be4fc12163bf7d6e82f47bba4984ddceec
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-09-24 12:01:51 -0400

    Update postscripttasks.sh

--- postscripttasks.sh
+++ postscripttasks.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+LOG=./logs/main.log
+function print() {
+  echo "$1" | sudo tee -a $LOG 
+}
 
 #Privilege Escalation
 print "sudo visudo /etc/sudoers, add/fix to 'Defaults use_pty'"
@@ -9,8 +13,8 @@ print "Make sure hashing algorithm set in pam.unix.so is sha512 or yescrypt in p
 
 #Strange User IDs
 print "Check for strange users:"
-mawk -F: '$3 < 1000 || $3 > 65533 {print $1, $3}' /etc/passwd
+mawk -F: '$3 < 1000 || $3 > 65533 {print $1, $3}' /etc/passwd >> $LOG
 
 #Insecure Files
 print "Check files with a permission of 700-777:"
-ls -l | grep "^-rw[x-]*"
+ls -l | grep "^-rw[x-]*" >> $LOG
