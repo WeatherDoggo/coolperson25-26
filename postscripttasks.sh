6996bb480a7bcced7d90403a2de0a5bf976aabcb
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-09-24 11:11:08 -0400

    Update postscripttasks.sh

--- postscripttasks.sh
+++ postscripttasks.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+#Privilege Escalation
+print "sudo visudo /etc/sudoers, add/fix to 'Defaults use_pty'"
+print "sudo visudo /etc/sudoers, add/fix to Defaults 'env_reset, timestamp_timeout=15'"
+print "sudo visudo /etc/sudoers, add 'Defaults logfile =''/var/log/sudo.log''' (DOUBLE QUOTES) "
+print "Remove all instances of NOPASSWD and !authenticate in /etc/sudoers"
+print "Make sure hashing algorithm set in pam.unix.so is sha512 or yescrypt in pam.d (pg 647 for specific file script)"
+
