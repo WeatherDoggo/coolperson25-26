2d4aa4cf157c70dfde2007b32988d3670cc1b130
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-09-24 16:41:20 -0400

    Update apache2.sh

--- conditional/apache2.sh
+++ conditional/apache2.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+LOG=./logs/main.log
+function print() {
+  echo "$1" | sudo tee -a $LOG 
+}
 
+if [[ $apache2needed == "yes" || $apache2needed == "y" ]];
+then
+	ufw allow http >> $LOG
+ 	ufw allow https >> $LOG
+	print "HTTP and HTTPS ports opened."
+ 	cp /etc/apache2/apache2.conf ../backups/apache2.conf
+  chmod 777 ../backups/apache2.conf
+  print "apache2.conf backed up."
+	cp importfiles/apache2.conf /etc/apache2/apache2.conf
+ 	print "apache2.conf configured."
+  chown root:root /etc/apache2/apache2.conf >> $LOG
+  chmod 644 /etc/apache2/apache2.conf >> $LOG
+  "apache2.conf ownership and permissions set."
+else
+	systemctl stop apache2.socket apache2.service >> $LOG 2>>$LOG
+  apt-get purge apache2 -y -qq >> $LOG
+  print "apache2 removed."
+fi
