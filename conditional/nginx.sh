548ae96fbf34549feca490256491b633bffb247f
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-09-24 12:01:03 -0400

    Update nginx.sh

--- conditional/nginx.sh
+++ conditional/nginx.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-cp /etc/nginx/nginx.conf $BACKUPDIR/nginx.conf
-chmod 777 $BACKUPDIR/nginx.conf
-print "nginx.conf backed up."
-cp importfiles/nginx.conf /etc/nginx/nginx.conf
-chmod 600 /etc/nginx/nginx.conf
-print "nginx.conf permissions configured."
-echo "... (add more stuff)"
+LOG=./logs/main.log
+function print() {
+  echo "$1" | sudo tee -a $LOG 
+}
 
-elif [[ $nginx == "no" || $nginx == "n" ]];
+if [[ $nginxneeded == "yes" || $nginxneeded == "y" ]];
 then
-	systemctl stop nginx.service >> $LOG_FILE 2>>$LOG_FILE
-	apt-get purge nginx nginx-full nginx-extras -y -qq >> $LOG_FILE
- 	printlog "nginx removed."
+	cp /etc/nginx/nginx.conf ../backups/nginx.conf
+	chmod 777 ../backups/nginx.conf
+	print "nginx.conf backed up."
+
+	cp importfiles/nginx.conf /etc/nginx/nginx.conf
+	chmod 600 /etc/nginx/nginx.conf
+	print "nginx.conf permissions configured."
+	systemctl restart nginx >> $LOG
+	print "nginx restarted with new configurations."
+else
+	systemctl stop nginx.service >> $LOG 2>>$LOG
+	apt-get purge nginx nginx-full nginx-extras -y -qq >> $LOG
+ 	print "nginx removed."
+fi
