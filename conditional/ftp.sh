68a9a04b6d0a4a16fec6a3e2122882025dce7e88
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-10-09 16:16:49 -0400

    Update ftp.sh

--- conditional/ftp.sh
+++ conditional/ftp.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+LOG=./logs/main.log
+function print() {
+  echo "$1" | sudo tee -a $LOG 
+}
 
+if [[ $ftpneeded == "yes" || $ftpneeded == "y" ]];
+then
+	#cp /etc/nginx/nginx.conf ../backups/nginx.conf
+	#chmod 777 ../backups/nginx.conf
+	#print "nginx.conf backed up."
+
+	#cp importfiles/nginx.conf /etc/nginx/nginx.conf
+	#chown root:root /etc/nginx/nginx.conf >> $LOG
+	#chmod 600 /etc/nginx/nginx.conf >> $LOG
+	#print "nginx.conf configured."
+	#systemctl reload nginx >> $LOG
+	#print "nginx restarted with new configurations."
+else
+	systemctl stop vsftpd.service >> $LOG 2>>$LOG
+	apt-get purge vsftpd -y -qq >> $LOG
+ 	print "vsftpd removed."
+fi
