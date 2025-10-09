8de4be60fd9183bbcbbe7efb1112fc661c5a0412
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-10-09 17:34:54 -0400

    Update postscripttasks.sh

--- postscripttasks.sh
+++ postscripttasks.sh
@@ -3,7 +3,11 @@ LOG=./logs/main.log
 function print() {
   echo "$1" | sudo tee -a $LOG 
 }
-print "PAM-AUTH-UPDATE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+#Strange User IDs
+print "Check for strange users:"
+mawk -F: '$3 < 1000 || $3 > 65533 {print $1, $3}' /etc/passwd >> $LOG
+
 #Privilege Escalation
 print "sudo visudo /etc/sudoers, add/fix to 'Defaults use_pty'"
 print "sudo visudo /etc/sudoers, add/fix to Defaults 'env_reset, timestamp_timeout=15'"
@@ -11,10 +15,5 @@ print "sudo visudo /etc/sudoers, add Defaults logfile =''/var/log/sudo.log''"
 print "Remove all instances of NOPASSWD and !authenticate in /etc/sudoers"
 print "Make sure hashing algorithm set in pam.unix.so is sha512 or yescrypt in pam.d (pg 647 for specific file script)"
 
-#Strange User IDs
-print "Check for strange users:"
-mawk -F: '$3 < 1000 || $3 > 65533 {print $1, $3}' /etc/passwd >> $LOG
-
-#Insecure Files
-print "Check files with a permission of 700-777:"
-ls -l | grep "^-rw[x-]*" >> $LOG
+print "PAM-AUTH-UPDATE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+print "RESTART THE COMPUTER FOR GRUB CONFIG!!!"
