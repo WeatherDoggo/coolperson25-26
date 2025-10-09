3bb4d83f5bf191c43f4b81407119b5b5b304f650
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-10-09 17:07:03 -0400

    Create ftp.sh

---
+++ conditional/ftp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+LOG=./logs/main.log
+function print() {
+  echo "$1" | sudo tee -a $LOG 
+}
+
+if [[ $ftpneeded == "yes" || $ftpneeded == "y" ]];
+then
+	#cp /etc/nginx/nginx.conf ../backups/nginx.conf
+
+	systemctl enable ftp
+	systemctl restart ftp
+	#cp importfiles/nginx.conf /etc/nginx/nginx.conf
+
+else
+	systemctl stop ftp.service >> $LOG 2>>$LOG
+	apt-get purge ftp -y -qq >> $LOG
+ 	print "ftp removed."
+fi
