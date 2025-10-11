b18a1b84584d8c45b99f5e6182b3f36bf0a5b0b7
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-10-11 17:40:42 -0400

    Update postscripttasks.sh

--- postscripttasks.sh
+++ postscripttasks.sh
@@ -14,7 +14,8 @@ print "sudo visudo /etc/sudoers, add/fix to Defaults 'env_reset, timestamp_timeo
 print "sudo visudo /etc/sudoers, add Defaults logfile =''/var/log/sudo.log''"
 print "Remove all instances of NOPASSWD and !authenticate in /etc/sudoers"
 print "Make sure hashing algorithm set in pam.unix.so is sha512 or yescrypt in pam.d (pg 647 for specific file script)"
-print "Files to check:/n /etc/sudoers, /etc/pam.d/common-password & common-auth, tmp files, all unit files, grub.d/(custom?)
+print "Files to check:/n /etc/sudoers, /etc/pam.d/common-password & common-auth, tmp files, all unit files, grub.d/(custom?)"
+print "Check /etc/gdm(3)/custom.conf for improper AutomaticLogin"
 
 print "PAM-AUTH-UPDATE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 print "RESTART THE COMPUTER FOR GRUB CONFIG!!!"
