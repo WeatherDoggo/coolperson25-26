a846435a0411f02b0249e362a5a601d50fbd7434
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-10-11 17:22:42 -0400

    Update auditing.sh

--- auditing.sh
+++ auditing.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+LOG=./logs/main.log
+function print() {
+  echo -e "$1" | sudo tee -a "$LOG" 
+}
 
+apt-get install auditd audispd-plugins
+systemctl enable auditd
+systemctl start auditd
+
+curl -o /etc/audit/rules.d/audit.rules https://raw.githubusercontent.com/Neo23x0/auditd/master/audit.rules
+augenrules --load
+systemctl restart auditd
