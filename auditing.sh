53a94ef451fe5982a2ea8058fd4bccc5116247f8
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-10-12 14:58:50 -0400

    Update auditing.sh

--- auditing.sh
+++ auditing.sh
@@ -4,7 +4,7 @@ function print() {
   echo -e "$1" | sudo tee -a "$LOG" 
 }
 
-apt-get install auditd audispd-plugins
+apt-get install auditd audispd-plugins -y -qq
 systemctl enable auditd
 systemctl start auditd
 print "auditd installed, enabled, and started. More work to be done here!!!!"
