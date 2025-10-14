adbdbd09fb9c44d4b73e9c8bb71624d63b24ce75
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-10-14 10:18:29 -0400

    Create squid.sh

---
+++ conditional/squid.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+LOG=./logs/main.log
+function print() {
+  echo "$1" | sudo tee -a $LOG 
+}
+
+if [[ $squidneeded == "yes" || $squidneeded == "y" ]];
+then
+	#cp /etc/nginx/nginx.conf ../backups/nginx.conf
+	print "CONFIGURE SQUID!!!!!!!!!!!!!!!!!"
+	#cp importfiles/nginx.conf /etc/nginx/nginx.conf
+
+else
+	systemctl stop squid >> $LOG 2>>$LOG
+	apt-get purge squid -y -qq >> $LOG
+ 	print "squid removed."
+fi
