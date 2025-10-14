68253750e5945bcb4c6da301b94c822c6181cba9
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-10-14 09:47:30 -0400

    Update ssh.sh

--- conditional/ssh.sh
+++ conditional/ssh.sh
@@ -14,11 +14,15 @@ then
 	chmod u-x,og-rwx /etc/ssh/sshd_config >> $LOG
 	chown root:root /etc/ssh/sshd_config >> $LOG
 	print "sshd_config permissions configured."
+
+	ufw allow OpenSSH
+	print "UFW rule added for ssh."
+	
 	systemctl enable sshd >> $LOG
  	systemctl restart sshd >> $LOG
 	print "SSH restarted."
 	print "SSH default port changed, PermitRootLogin set to no, MaxAuthTries set to 3, Client closes after 4 minutes inactive, LoginGraceTime set to 20, PermitEmptyPasswords is set to no, HostBasedAuthentication set to no, and StrictModes is set to yes."
- 	print "Optional SSH tasks include MaxSessions, TCPKeepAlive, & changing default port."
+ 	print "Optional SSH tasks include MaxSessions & TCPKeepAlive."
 else
 	apt-get purge openssh-server openssh-client -y -qq >> $LOG
 	ufw deny ssh >> $LOG
