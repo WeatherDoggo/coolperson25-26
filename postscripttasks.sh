eb2c96812022c52c0a6d27069bdff96039fbe277
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-11-10 10:44:17 -0500

    Update postscripttasks.sh

--- postscripttasks.sh
+++ postscripttasks.sh
@@ -8,22 +8,5 @@ function print() {
 print "Check for strange users:"
 mawk -F: '$3 < 1000 || $3 > 65533 {print $1, $3}' /etc/passwd >> $LOG
 
-#Privilege Escalation
-print "sudo visudo /etc/sudoers, add/fix to 'Defaults use_pty'"
-print "sudo visudo /etc/sudoers, add/fix to Defaults 'env_reset, timestamp_timeout=15'"
-print "sudo visudo /etc/sudoers, add Defaults logfile =''/var/log/sudo.log''"
-print "Remove all instances of NOPASSWD and !authenticate in /etc/sudoers"
-print "Make sure to config browser properly."
-print "Make sure hashing algorithm set in pam.unix.so is sha512 or yescrypt in pam.d (pg 647 for specific file script)"
-print "Set screen timeout policy to 5 minutes or less"
-print "Enable automatic screen lock"
-print "Enable automatic check for updates"
-print "In /etc/grub.d/40_custom, set check_signatures=enforce"
-print "Files to check:/n /etc/sudoers, /etc/pam.d/common-password & common-auth, tmp files, all unit files, grub.d/(custom_10,40,more?), /etc/apt/apt.conf, /etc/apt/apt.conf.d, /etc/apt/sources.list, /etc/apt/sources.list.d"
-print "Ensure all required services are running"
-print "Check if the kernel or GRUB needs to be updated, then run    sudo apt dist-upgrade followed by     sudo update-grub"
-print "PAM-AUTH-UPDATE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-print "RESTART THE COMPUTER FOR GRUB CONFIG!!!"
-print ""
-print "COMPLETE THE TASKS ABOVE."
+print "COMPLETE THE TASKS ON THE CHECKLIST!"
 read confirm
