8e67f9f51cbaa143ae932fb422dc4c937502800b
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-11-11 14:04:09 -0500

    Update apparmor.sh

--- apparmor.sh
+++ apparmor.sh
@@ -6,10 +6,9 @@ function print() {
 
 print "Installing apparmor..."
 apt-get install apparmor apparmor-utils apparmor-profiles apparmor-profiles-extra apparmor-notify -y -qq >> $LOG
-systemctl start apparmor >> $LOG
-systemctl enable apparmor >> $LOG
+systemctl enable apparmor.service --now >> $LOG
 aa-status >> $LOG
 aa-enforce /etc/apparmor.d/* >> $LOG
 aa-remove-unknown >> $LOG
-systemctl restart apparmor | sudo tee -a "$LOG" 
+systemctl restart apparmor.service | sudo tee -a "$LOG" 
 print "Apparmor configured to enforce default profiles and remove unknown profiles. Additional manual profiles may need to be added."
