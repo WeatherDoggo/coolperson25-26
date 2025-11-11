9ef8a080f8e6f83e04cabe133d2e189ff0f3a24b
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-11-11 13:22:29 -0500

    Create logs.sh

---
+++ logs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+LOG=./logs/main.log
+function print() {
+  echo -e "$1" | sudo tee -a "$LOG" 
+}
+
+apt-get install rsyslog -y -qq >>$LOG
+systemctl enable rsyslog.service --now
+print "rsyslog installed and enabled."
