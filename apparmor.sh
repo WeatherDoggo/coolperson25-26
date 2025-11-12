5756f5fa2d0e9c600147e68ee322de696e5ee80c
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-11-12 08:45:02 -0500

    Update apparmor.sh

--- apparmor.sh
+++ apparmor.sh
@@ -10,5 +10,5 @@ systemctl enable apparmor.service --now >> $LOG
 aa-status >> $LOG
 aa-enforce /etc/apparmor.d/* >> $LOG
 aa-remove-unknown >> $LOG
-systemctl restart apparmor.service | sudo tee -a "$LOG" 
+systemctl restart apparmor.service >> $LOG
 print "Apparmor configured to enforce default profiles and remove unknown profiles. Additional manual profiles may need to be added."
