4fc94bd0c0ad215030f8f2576857fd5619eace05
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-11-18 12:33:03 -0500

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -11,6 +11,7 @@ print "rsyslog installed and enabled."
 chgrp adm /var/log/syslog
 chown syslog /var/log/syslog
 
+#Auditd
 apt-get install auditd audispd-plugins -y -qq
 systemctl enable auditd.service --now
 cp ./importfiles/auditd.conf /etc/audit/auditd.conf
