cf9376cd5dc7a2c2982169ce5f81016d2a79cd41
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-11-20 10:44:22 -0500

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -15,6 +15,8 @@ ufw default allow outgoing >> $LOG
 ufw default deny routed >> $LOG
 ufw logging on
 ufw deny 1337 >> $LOG
+
+#loopback config
 ufw allow in on lo
 ufw allow out on lo
 ufw deny in from 127.0.0.0/8
@@ -22,3 +24,21 @@ ufw deny in from ::1
 systemctl enable ufw.service --now >> $LOG
 print "Firewall enabled, port 1337 closed, and loopback traffic is configured."
 #MAKE IT SO LISTENING PROCESSES IS CHECKED BEFORE CLOSING PORT 1337!!!!!!!
+
+{
+  unset a_ufwout;unset a_openports
+  while read -r l_ufwport; do
+    [ -n "$l_ufwport" ] && a_ufwout+=("$l_ufwport")
+  done < <(ufw status verbose | grep -Po '^\h*\d+\b' | sort -u)
+  while read -r l_openport; do
+    [ -n "$l_openport" ] && a_openports+=("$l_openport")
+  done < <(ss -tuln | awk '($5!~/%lo:/ && $5!~/127.0.0.1:/ && $5!~/\[?::1\]?:/) {split($5, a, ":"); print a[2]}' | sort -u)
+  a_diff=("$(printf '%s\n' "${a_openports[@]}" "${a_ufwout[@]}" "${a_ufwout[@]}" | sort | uniq -u)")
+  if [[ -n "${a_diff[*]}" ]]; then
+    echo -e "\n- Audit Result:\n ** FAIL **\n- The following port(s) don't have a rule in UFW: $(printf '%s\n' \\n"${a_diff[*]}")\n- End List"
+  else
+    echo -e "\n - Audit Passed -\n- All open ports have a rule in UFW\n"
+  fi
+}
+print "For each port in the audit that does not have a firewall rule, evaluate the service listening port and add a rule accepting or denying inbound connections (ex. ufw allow in <port>/<tcp or udp protocol>)
+read portconfirm
