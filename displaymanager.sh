6dc0b22c6ef3fe717f6492c7d79c5ab12c21944d
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-11-21 09:42:45 -0500

    Update displaymanager.sh

--- displaymanager.sh
+++ displaymanager.sh
@@ -7,11 +7,28 @@ function print() {
 if [[ $OS == "mint" ]]; then
   print "Configure lightdm!"
 elif [[ $OS == "ubuntu" ]]; then
+  #mkdir -p /etc/dconf/
+  #echo -e "1. Add/check that /etc/dconf/profile/ contains the following:\nuser-db:user\nsystem-db:{NAME_OF_DCONF_DATABASE}\n2. Create the directory /etc/dconf/db/local.d/ if it doesn't already exist\n 3. Create the key file /etc/dconf/db/local.d/00-screensaver to provide
+#information for the local database; ex file in importfiles\nDid this need to be done?"
+  #read GDMdatabase
+  
+  #print "you must logout and restart computer for changes to take effect."
+  
   cp importfiles/custom.conf /etc/gdm/custom.conf
   cp importfiles/custom.conf /etc/gdm3/custom.conf
-  
-  cp importfiles/00-screensaver /etc/dconf/db/local.d/00-screensaver
-  print "screensaver automatically set after 5 minutes."
-  
+
+  #Screensaver
+  #gsettings set org.gnome.desktop.screensaver lock-delay 5
+  #gsettings set org.gnome.desktop.session idle-delay 300
+
+  #Banner
+  #gsettings set org.gnome.login-screen banner-message-text 'Authorized uses only. All activity may be monitored and reported'
+  #gsettings set org.gnome.login-screen banner-message-enable true
+
+  #Disable login user list
+  #gsettings set org.gnome.login-screen disable-user-list true
+
+  dconf update
+  #print "ADD A USER PROFILE FOR EACH!!!!"
   print "gdm & gdm3 custom.conf configured. More work to be done!!!!"
 fi
