9b8a7f322b89b2bdbd890c03b0dcbc5aac4f3d7e
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-11-21 13:42:03 -0500

    Create scap.sh

---
+++ scap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+LOG=./logs/main.log
+function print() {
+  echo -e "$1" | sudo tee -a "$LOG" 
+}
+
+#Installing OpenSCAP-Workbench
+apt-get install pkg-config cmake g++ git libopenscap-dev qtbase5-dev qt5-qmake libqt5xmlpatterns5-dev -y -qq >> $LOG
+git clone https://github.com/OpenSCAP/scap-workbench.git
+cd scap-workbench
+touch ~/scap-workbench/doc/user_manual.html
+sudo rm -rf build
+mkdir build
+cd build
+cmake ../
+make
+make install
+
+#check files
+git clone -b master https://github.com/ComplianceAsCode/content.git
+cd content/build/
+cmake ../
+make -j4
+
+
+print "scap-workbench should be ready for use. Use the ds file within ~/content/build for ubuntu 24."
