c7aedf37d6993612c2280bae0cee07f26aea583c
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-12-09 18:34:00 +0000

    Implement xdmcp disablement in GDM configuration
    Add logic to disable xdmcp in GDM by modifying custom.conf.

--- displaymanager.sh
+++ displaymanager.sh
@@ -28,6 +28,28 @@ elif [[ $OS == "ubuntu" ]]; then
   #Disable login user list
   #gsettings set org.gnome.login-screen disable-user-list true
 
+  #Disable xdmcp in GDM (Remediation is applicable only in certain platforms)
+  if dpkg-query --show --showformat='${db:Status-Status}' 'gdm3' 2>/dev/null | grep -q '^installed$'; then
+
+    #Try find '[xdmcp]' and 'Enable' in '/etc/gdm3/custom.conf', if it exists, set to 'false', if it isn't here, add it, if '[xdmcp]' doesn't exist, add it there
+    if grep -qzosP '[[:space:]]*\[xdmcp]([^\n\[]*\n+)+?[[:space:]]*Enable' '/etc/gdm3/custom.conf'; then
+    
+      sed -i "s/Enable[^(\n)]*/Enable=false/" '/etc/gdm3/custom.conf'
+    elif grep -qs '[[:space:]]*\[xdmcp]' '/etc/gdm3/custom.conf'; then
+      sed -i "/[[:space:]]*\[xdmcp]/a Enable=false" '/etc/gdm3/custom.conf'
+    else
+      if test -d "/etc/gdm3"; then
+        printf '%s\n' '[xdmcp]' "Enable=false" >> '/etc/gdm3/custom.conf'
+      else
+        print "Config file directory '/etc/gdm3' doesnt exist, not remediating, assuming non-applicability." >&2
+      fi
+    fi
+
+    else
+    >&2 print 'Remediation is not applicable, nothing was done'
+    fi
+
+
   dconf update
   #print "ADD A USER PROFILE FOR EACH!!!!"
   print "gdm & gdm3 custom.conf configured. More work to be done!!!!"
