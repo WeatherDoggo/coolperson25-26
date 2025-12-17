328de99a819059a4e1f4610712bef22a728b7d6a
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-12-17 11:07:13 -0500

    Update php.sh

--- conditional/php.sh
+++ conditional/php.sh
@@ -1,2 +1,14 @@
-#1/bin/bash
-#stuff goes here
+#!/bin/bash
+LOG=./logs/main.log
+function print() {
+  echo "$1" | sudo tee -a $LOG 
+}
+
+if [[ $phpneeded == "yes" || $phpneeded == "y" ]];
+then
+	print "CONFIGURE PHP!!!!!!!!!!!!!!!!!"
+else
+  systemctl stop php
+  apt-get purge php -y -qq >> $LOG
+  print "php removed."
+fi
