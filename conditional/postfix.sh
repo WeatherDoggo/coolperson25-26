2e35ed591e1437c8c976a51bb4055d9ec766211b
WeatherDoggo <andrew.c.patrone@gmail.com>
2025-12-17 10:03:40 -0500

    Update postfix.sh

--- conditional/postfix.sh
+++ conditional/postfix.sh
@@ -1,2 +1,15 @@
 #1/bin/bash
-#stuff does things
+#!/bin/bash
+LOG=./logs/main.log
+function print() {
+  echo "$1" | sudo tee -a $LOG 
+}
+
+if [[ $postfixneeded == "yes" || $postfixneeded == "y" ]];
+then
+	print "CONFIGURE POSTFIX!!!!!!!!!!!!!!!!!"
+else
+  systemctl stop postfix
+  apt-get purge postfix
+  print "postfix mailserver removed."
+fi
