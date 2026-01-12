1623c7dcb723eef375d070262cb6b35b8ba2de46
WeatherDoggo <andrew.c.patrone@gmail.com>
2026-01-12 11:45:00 -0500

    Update manualscans.sh

--- manualscans.sh
+++ manualscans.sh
@@ -5,33 +5,33 @@ function print() {
 }
 
 mkdir ./scans/
-chmod 777 ./scans/
+chmod 666 ./scans/
 print "Running scans and sending results to /scans/..."
 
 print "Applications with hack, crack, or evil in the name:\n" > ./scans/hackcrack.txt
 dpkg -l | grep -E 'hack|crack|evil' >> ./scans/hackcrack.txt
-chmod 777 ./scans/hackcrack.txt
+chmod 666 ./scans/hackcrack.txt
 print "Apps with hack or crack have been scanned for."
 
 #All Installed Apps
 apt list --installed > ./scans/aptapps.txt
-chmod 777 ./scans/aptapps.txt
+chmod 666 ./scans/aptapps.txt
 print "apt installed apps listed in aptapps.txt."
 
 ss -tulnp > ./scans/ss_-tulnp.txt
-chmod 777 ./scans/ss_-tulnp.txt
+chmod 666 ./scans/ss_-tulnp.txt
 print "Ran ss -tulnp to scan listening ports."
 
 ps aux > ./scans/ps_aux.txt
-chmod 777 ./scans/ps_aux.txt
+chmod 666 ./scans/ps_aux.txt
 print "Ran ps aux to see running processes."
 
 systemctl list-unit-files --type=service > ./scans/list-unit-files.txt
-chmod 777 ./scans/list-unit-files.txt
+chmod 666 ./scans/list-unit-files.txt
 print "Ran systemctl list-unit-files to see all processes on the VM."
 
 lsof -i -P -n > ./scans/lsof.txt
-chmod 777 ./scans/lsof.txt
+chmod 666 ./scans/lsof.txt
 print "Ran lsof to list all open network connections and the processes that opened them."
 
 print "PIDpaths"  > ./scans/PIDpaths.txt
@@ -40,53 +40,59 @@ for pid in $(ls /proc | grep -E '^[0-9]+$'); do
     then print "PID: $pid, Command: $(readlink -f /proc/$pid/exe)" >> ./scans/PIDpaths.txt
     fi 
   done
-chmod 777 ./scans/PIDpaths.txt
+chmod 666 ./scans/PIDpaths.txt
 print "Resolved all of the executible paths of PIDS in PIDpaths.txt. Look for ones in /tmp, /dev/shm, or ones that have been deleted but are still running." >> ./scans/PIDpaths.txt
 
 find / -type f -perm /6000 -ls > ./scans/binaryconfigs.txt
-chmod 777 ./scans/binaryconfigs.txt
+chmod 666 ./scans/binaryconfigs.txt
 print "Listed all SUID/SGID binaries in binaryconfigs.txt to check for misconfiguration."
 print "Watch for the following:\nSetuid on scripts (.sh, .py, etc.)\nBinaries in user-writable paths like /tmp, /home, or /var/tmp\nBinaries with 777 permissions" >> ./scans/binaryconfigs.txt
 
 cat /etc/hosts > ./scans/hostentries.txt
-chmod 777 ./scans/hostentries.txt
+chmod 666 ./scans/hostentries.txt
 print "Check for any unusual entries that redirect traffic to malicious IPs." >> ./scans/hostentries.txt
 
 cat /etc/resolv.conf > ./scans/suspiciousDNS.txt
-chmod 777 ./scans/suspiciousDNS.txt
+chmod 666 ./scans/suspiciousDNS.txt
 print "Check for suspicious DNS servers that could be redirecting traffic." >> ./scans/suspiciousDNS.txt
 
 cat /etc/crontab /etc/cron.*/* > ./scans/cronjobs.txt
-chmod 777 ./scans/cronjobs.txt
+chmod 666 ./scans/cronjobs.txt
 print "crontab & cron.* jobs listed in scans/cronjobs.txt."
 
 locate *.zip > ./scans/zippaths.txt
-chmod 777 ./scans/zippaths.txt
+chmod 666 ./scans/zippaths.txt
 print ".zip file paths listed in zippaths.txt."
 
 mawk -F: '$3 < 1000 || $3 > 65533 {print $1, $3}' /etc/passwd > ./scans/strangeusers.txt
-chmod 777 ./scans/strangeusers.txt
+chmod 666 ./scans/strangeusers.txt
 print "Strange UIDs listed in strangeusers.txt"
 
 #Hidden Files
 find / -type f -name ".*" > ./scans/hiddenfiles.txt
-chmod 777 ./scans/hiddenfiles.txt
+chmod 666 ./scans/hiddenfiles.txt
 print "Hidden files listed in hiddenfiles.txt"
 
 #Files without an owner
 find / -xdev \( -nouser -o -nogroup \) -print > ./scans/filesnoowner.txt
-chmod 777 ./scans/filesnoowner.txt
+chmod 666 ./scans/filesnoowner.txt
 print "Files with no owning user/group listed in filesnoowner.txt"
 
 #Insecure files and directories
 find / -xdev -type f -perm -002 -ls > ./scans/writeablethings.txt
-chmod 777 ./scans/writeablethings.txt
+chmod 666 ./scans/writeablethings.txt
 find / -xdev -type d -perm -002 -ls >> ./scans/writeablethings.txt
 print "Files and directories that need securing have been stored in writeablethings.txt."
 
-#mkdir ./scans/diffs
-#diff -B -i ./scans/$file ./logs/cleanscans/$file >> ./scans/diffs/diff$file
-#chmod 770 ./scans/diffs/diff$file
+#diff files
+mkdir ./scans/diffs
+chmod 666 ./scans/diffs
+function filediff () {
+  local file="$1"
+  diff -B -i ./scans/$file ./logs/cleanscans/$file >> ./scans/diffs/diff$file
+  chmod 666 ./scans/diffs/diff$file
+}
+print "diffs created for scans."
 
 #Confirmation before continuting with other scripts
 print "Have you reviewed the scans?"
