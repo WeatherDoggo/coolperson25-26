8024a16675fec6018a876cd0cc05dc9591797e62
WeatherDoggo <andrew.c.patrone@gmail.com>
2026-01-12 14:05:46 -0500

    Update backup.sh

--- logs/backup.sh
+++ logs/backup.sh
@@ -19,10 +19,10 @@ backup /etc/security/faillock.conf
 backup /etc/security/pwquality.conf
 backup /etc/sysctl.conf
 backup /etc/security/limits.conf
-if [[ "$OS" == "mint" ]]; then
-  backup /etc/linuxmint/mintupdate.conf
-elif [[ $OS == "ubuntu" ]]; then
-  backup /etc/gdm/custom.conf
-  backup /etc/gdm3/custom.conf
-fi
+#if [[ "$OS" == "mint" ]]; then
+#backup /etc/linuxmint/mintupdate.conf
+#elif [[ $OS == "ubuntu" ]]; then
+#  backup /etc/gdm/custom.conf
+#  backup /etc/gdm3/custom.conf
+#fi
 print "Critical files backed up. More files may need to conditionally be backed up depending on the application!"
