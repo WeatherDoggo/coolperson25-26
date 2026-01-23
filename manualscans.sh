77b1a85ac555a1e68b40123f8ccc3250f187b49f
WeatherDoggo <andrew.c.patrone@gmail.com>
2026-01-23 08:52:54 -0500

    Refactor manualscans.sh for kernel module handling
    Updated script to store kernel module descriptions in kerneldesc.txt and added functionality to filter out driver modules. Also created kernelpaths.txt for module file paths.

--- manualscans.sh
+++ manualscans.sh
@@ -91,15 +91,31 @@ chmod 777 ./scans/writeablethings.txt
 find / -xdev -type d -perm -002 -ls >> ./scans/writeablethings.txt
 print "Files and directories that need securing have been stored in writeablethings.txt."
 
-: > ./scans/kernelmodules.txt   # truncate output file
+: > ./scans/kerneldesc.txt   # truncate output file
 # Get each module name from /proc/modules
 awk '{print $1}' /proc/modules | while read -r mod; do
     # Get filename and description (may be empty)
     fname=$(modinfo -F filename "$mod" 2>/dev/null)
     desc=$(modinfo -F description "$mod" 2>/dev/null)
-    printf '%s\t%s\t%s\n' "$mod" "$fname" "$desc" >> "$out"
+    if echo "$desc" | grep -qi 'driver'; then
+    	continue
+    fi
+    printf '%s\t%s\t%s\n' "$mod" "$desc" >> ./scans/kerneldesc.txt
 done
-chmod 777 ./scans/kernelmoduleinfo.txt
+chmod 777 ./scans/kerneldesc.txt
+
+: > ./scans/resources/kernelpaths.txt   # truncate output file
+# Get each module name from /proc/modules
+awk '{print $1}' /proc/modules | while read -r mod; do
+    # Get filename and description (may be empty)
+    fname=$(modinfo -F filename "$mod" 2>/dev/null)
+    desc=$(modinfo -F description "$mod" 2>/dev/null)
+    if echo "$desc" | grep -qi 'driver'; then
+    	continue
+    fi
+printf '%s\t%s\t%s\n' "$mod" "$fname" >> ./scans/resources/kernelpaths.txt
+done
+chmod 777 ./scans/resources/kernelpaths.txt
 print "kernel module filepaths and descriptions printed."
 #diff files
 #mkdir ./scans/diffs
